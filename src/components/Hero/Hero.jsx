f72af5704a1dbf87b60b801b6aa6811ffade5293
Shreyas0017 <shreyassaha00@gmail.com>
2025-03-05 01:08:33 +0530

    hero.css changed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
   return (
     <div className='hero'>
       <img src={Profile} alt="" />
-      <h1>I am Shreyas Saha, a random debugger</h1>
-      <p>Description</p>
+      <h1><span>I am Shreyas Saha, </span>a random debugger.</h1>
+      <p>I am a passionate and skilled web and app developer, currently in my second year of Computer Science and Engineering. With hands-on experience in modern development technologies, I have worked on projects ranging from full-stack web applications to AI-powered tools.</p>
       <div className="hero-action">
         <div className="hero-connect">Connect with me</div>
         <div className="hero-resume">My resume</div>
